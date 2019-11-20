73a70d56fcf730ebe559dc36cedc95a394b1e198
naz_liton <nazrulliton@icloud.com>
2019-11-20 14:08:23 +0000

    passes film title test

--- lab_enumeration_callbacks_films_start/models/cinema.js
+++ lab_enumeration_callbacks_films_start/models/cinema.js
@@ -2,4 +2,16 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+Cinema.prototype.getFilmTitles = function () {
+  const result = this.films.map((film)=>{
+    return film.title;
+
+  });
+  return result;
+};
+
+
+
+
+
 module.exports = Cinema;
